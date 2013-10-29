b4862dd427de6662bb6410494d3fe4ee0d3d63fe
root <root@ludi.tiamat>
2013-10-30 00:57:04 +0200

    Simple fill on cardinal directions

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ dafence
 
 A simple game of conquest.
 
-Hardly anything here ATM, but you can run server.py, which will run a local server on port 8000, and then open a browser at http://0.0.0.0:8000 to see the board. Clicking on a cell will own it (currently as root, the only user) and dive into it. Which means you will not notice it is owned till you pop back up. Silly, I know.
+Hardly anything here ATM, but you can run server.py, which will run a local server on port 8000, and then open a browser at http://0.0.0.0:8000 to see the board. Click a cell to own it (currently as root, the only user). If there is another owned cell near by on the same row or column, you will also own all the cells between them - recursively. Right click a cell to zoom into it.
--- server.py
+++ server.py
@@ -4,36 +4,58 @@ class Player(object):
     def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.stakes = [None for i in range(20)]
 empty = Player('empty', 'transparent')
 players = {'empty': empty, 'root': Player('root', 'red')}
 
-class Board(object):
-    def __init__(self, parent, owner):
-        self.parent = self if parent is None else parent
+width = 10
+height = 10
+size = width * height
+horizon = (width + height) / 4
+class Cell(object):
+    def __init__(self, parent, pos, owner):
+        self.parent = parent
+        self.pos = pos
         self.owner = owner
-        self.cells = [owner for i in range(100)]
-    def getmap(self):
-        return [
-            (cell.owner.name, cell.owner.color) if type(cell) is Board
-            else (cell.name, cell.color)
-            for cell in self.cells
-        ]
+        self.board = None
+    def scan(self, interval):
+        collected = []
+        cellidx = self.pos[-1]
+        startmod = cellidx % width
+        cell = self.parent.cells[cellidx]
+        for i in range(horizon):
+            cellidx += interval
+            cellmod = cellidx % width
+            if 1 == interval and cellmod < startmod: break
+            if -1 == interval and cellmod > startmod: break
+            if cellidx < 0 or cellidx >= size: break
+            cellmod = cellidx
+            cell = self.parent.cells[cellidx]
+            if cell.owner.name != 'empty': break
+            collected.append(cell)
+        return cell, collected
     def claim(self, player):
         self.owner = player
-        return True
+        score = 1
+        for interval in (1, -1, width, -width):
+            cell, collected = self.scan(interval)
+            if cell.owner.name == player.name:
+                score += sum([cell.claim(player) for cell in collected])
+        return score
 
-        print i, user
-board = Board(None, empty)
+class Board(object):
+    def __init__(self, parent):
+        self.parent = parent
+        self.cells = [Cell(self, parent.pos + [i], empty) for i in range(size)]
+    def getmap(self): return [(cell.owner.name, cell.owner.color) for cell in self.cells]
+board = Board(Cell(None, [], empty))
 
 def getboard(pos):
     curboard = board
     for cellidx in pos:
-        cur = curboard.cells[cellidx]
-        if type(cur) is Player:
-            curboard.cells[cellidx] = Board(curboard, cur)
-            curboard = curboard.cells[cellidx]
-        else: curboard = cur
+        cell = curboard.cells[cellidx]
+        if cell.board is None:
+            cell.board = Board(cell)
+        curboard = cell.board
     return curboard
 
 from SimpleHTTPServer import SimpleHTTPRequestHandler
@@ -60,7 +82,7 @@ class Handler(SimpleHTTPRequestHandler):
                 try: name = query['name'][0]
                 except KeyError: data = {'error': 'No name specified'}
                 else:
-                    data = curboard.claim(players[name])
+                    data = curboard.parent.claim(players[name])
 
         try: data
         except NameError: data = {'error': 'Did not understand ' + url.path}
@@ -78,4 +100,6 @@ try:
 except:
     server.server_close()
     from sys import exc_info
-    if 'KeyboardInterrupt' != exc_info()[0].__name__: raise
+    if 'KeyboardInterrupt' != exc_info()[0].__name__:
+        print 'An unexpected error has occured:'
+        raise
